556fefcdf07e766c2db3ab65a6c5192dc50be9f3
Aditya <aditya.96.nair@gmail.com>
2024-04-14 13:05:41 +0530

    Second Commit

--- src/main/java/com/apptrove/TeamTracker/services/UserServiceImpl.java
+++ src/main/java/com/apptrove/TeamTracker/services/UserServiceImpl.java
@@ -2,56 +2,57 @@ package com.apptrove.TeamTracker.services;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.apptrove.TeamTracker.entities.User;
 import com.apptrove.TeamTracker.payload.ApiResponse;
 import com.apptrove.TeamTracker.repository.UserRepository;
 import com.apptrove.TeamTracker.utility.Utils;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Autowired
 	private Utils utils;
-	
+
 	@Override
-	public ApiResponse findByUsername(String username,String password) {
+	public ApiResponse findByUsername(String username, String password) {
 		User user = new User();
 		String response = null;
-		Map<String,Object> respObj = new HashMap<String,Object>();
+		Map<String, Object> respObj = new HashMap<String, Object>();
 		ApiResponse apiResponse = null;
 		try {
 			user = repository.findByUsername(username);
 			if (user == null) {
-				response = new String(username +" is not part of the administration. Kindly contact admin.");
+				response = new String(username + " is not part of the administration. Kindly contact admin.");
 				return new ApiResponse(response, false);
-			} 
-			
+			}
+
 			if (user.getLastLoginDate() == null) {
-				response = new String(username+" is already logged in.");
+				response = new String(username + " is already logged in.");
 				respObj = utils.userMapper(user);
-				return new ApiResponse(respObj,response, false);
+				return new ApiResponse(respObj, response, false);
 			}
-			
-			if(!user.getPassword().equals(password)) {
-				response = new String(username +" has entered wrong password. Kindly re-enter the correct password.");
+
+			if (!user.getPassword().equals(password)) {
+				response = new String(username + " has entered wrong password. Kindly re-enter the correct password.");
 				return new ApiResponse(response, false);
 			}
+
 			System.out.println(user);
 			respObj = utils.userMapper(user);
+			user.setLastLoginDate(null);
+			repository.save(user);
 			response = "User Login Successful";
 			apiResponse = new ApiResponse(respObj, response, true);
 		} catch (Exception e) {
 			response = new String(e.getMessage());
 			return new ApiResponse(response, false);
 		}
-		
+
 		return apiResponse;
 	}
 
