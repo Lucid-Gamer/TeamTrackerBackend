a6ac2c5138ce38f14a87aff9df8c605903154357
Aditya <aditya.96.nair@gmail.com>
2024-04-21 21:59:06 +0530

    Banner Completed

--- src/main/java/com/apptrove/TeamTracker/commons/VersionPrinter.java
+++ src/main/java/com/apptrove/TeamTracker/commons/VersionPrinter.java
@@ -3,15 +3,17 @@ package com.apptrove.TeamTracker.commons;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
-public class VersionController implements ApplicationRunner {
+@Component
+public class VersionPrinter implements ApplicationRunner {
 
     @Value("${app.version}")
     private String appVersion;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Application Version : " + this.appVersion);
+        // System.out.println("Application Version: " + appVersion);
     }
 
 }
